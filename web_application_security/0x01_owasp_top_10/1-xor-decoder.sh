bed6ad31d5df4b579f550ca13c45867fdeec9679
gastonperego <gastonperego@gmail.com>
2024-09-05 09:36:36 -0400

    feat: task 1

---
+++ web_application_security/0x01_owasp_top_10/1-xor-decoder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dpass=$(echo "$1" | sed "s/{xor}//" | base64 --decode)
+xor_pass=""
+
+for ((i = 0; i < ${#dpass}; i++)); do
+	char=${dpass:$i:1}
+	ascii=$(printf "%d" "'$char")
+	xor_digit=$(($ascii ^ 95))
+	xor_pass+="$(printf "$(printf '\\x%x' $xor_digit)")"
+done
+
+echo -e "$xor_pass"
+
