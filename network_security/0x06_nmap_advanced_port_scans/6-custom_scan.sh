f166c70ae65980fffacb97716d74162ef3d526e7
gastonperego <gastonperego@gmail.com>
2024-11-05 08:39:21 -0500

    fix: scanflags instead of scan-flags

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap --scan-flags URGACKPSHRSTSYNFIN -p$2 -oN custom_scan.txt $1 > /dev/null 2> /dev/null
+sudo nmap --scanflags URGACKPSHRSTSYNFIN -p$2 -oN custom_scan.txt $1 > /dev/null 2> /dev/null
