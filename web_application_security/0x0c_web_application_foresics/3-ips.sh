8209653cef134c0605ff12890ac3637aea91064b
gastonperego <gastonperego@gmail.com>
2024-11-24 21:55:09 -0300

    fix: string to search for

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "BREAK" auth.log | grep -Eo '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sort -u | wc -l
+grep "Accepted password for root" auth.log | grep -Eo '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sort -u | wc -l
