80142a9a0e715583e92c23192917190e7288b977
gastonperego <gastonperego@gmail.com>
2024-12-15 19:19:48 -0300

    fix: print format

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -3,6 +3,8 @@ require 'net/http'
 
 def get_request(url)
   uri = URI(url)
-  data = Net::HTTP.get(uri)
-  puts "#{data}"
+  data = Net::HTTP.get_response(uri)
+  puts "Response status: #{data.code} #{data.message}"
+  puts "Response body:
+#{data.body}"
 end
