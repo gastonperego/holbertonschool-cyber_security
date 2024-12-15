38cd6ed23e0558ba9eb1cac4a6fd1ced5de648bc
gastonperego <gastonperego@gmail.com>
2024-12-15 19:34:28 -0300

    fix: no arguments given

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 def print_arguments
   num = 0
-  puts "Arguments:"
-  ARGV.each do |entry|
-    num += 1
-    puts "#{num}. #{ARGV[num - 1]}"
+  if ARGV.length > 0
+    puts "Arguments:"
+    ARGV.each do |entry|
+      num += 1
+      puts "#{num}. #{ARGV[num - 1]}"
+    end
   end
 end
