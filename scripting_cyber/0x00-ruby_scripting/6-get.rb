eaaa7aef61b87d5d087f8078230246effbbae5a6
gastonperego <gastonperego@gmail.com>
2024-12-15 19:25:31 -0300

    fix: print format

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'json'
+require 'uri'
 
 def get_request(url)
-  uri = URI(url)
-  data = Net::HTTP.get_response(uri)
-  puts "Response status: #{data.code} #{data.message}"
-  puts "Response body:
-#{data.body}\n"
+  uri = URI.parse(url)
+  response = Net::HTTP.get_response(uri)
+
+  puts "Response status: #{response.code} #{response.message}"
+
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
 end
